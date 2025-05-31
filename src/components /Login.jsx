44f9bc3837fcbc0400bedcf56280dc1351fbb92c
AkashItlogiko <akashsahanwu089@gmail.com>
2025-05-31 20:35:46 +0600

    P

--- src/components /Login.jsx
+++ src/components /Login.jsx
@@ -1,36 +1,24 @@
-import React from "react";
-
-const Login = () => {
-  return (
-    <div  >
-      <h1>Login</h1>
-      <form>
-        <div  >
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" name="email" placeholder="Enter your email" required />
-        </div>
-        <div  >
-          <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" name="password" placeholder="Enter your password" required />
-        </div>
-        <div  >
-          <input type="checkbox" id="rememberMe" name="rememberMe" />
-          <label htmlFor="rememberMe">Remember Me</label>
-        </div>
-        <div  >
-          <span href="/forgot-password"  >
-            Forgot Password?
-          </span>
-        </div>
-        <button type="submit"  >
-          Login
-        </button>
+ import React from 'react';
+ 
+ const Login = () => {
+    return (
         <div>
-        <span>New Hare? <Link to=''></Link></span>
-        </div>
-      </form>
-    </div>
-  );
-};
+             <div className='bg-slate-800 border border-slate-600 rounded-md p-8 shadow-lg backdrop-filer backdrop-blur-lg bg-opacity-30 relative'>
+                <h1 className='text-4xl font-bold text-center'>Login</h1>
+                <form action="">
+                  <div className='relative my-4'>
+                   <input type="text" className='block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus-border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder=''  />
+                     <label htmlFor=""className='absolute text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500'>Username</label> 
+                 </div>  
 
-export default Login;
+                  <div className='relative my-4'>
+                   <input type="password" className='block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus-border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder=''  />
+                     <label htmlFor=""className='absolute text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500'>Your Password</label> 
+                 </div>  
+                </form>
+             </div>
+        </div>
+    );
+ };
+ 
+ export default Login;
