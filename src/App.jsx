d37527bbe93f4f2a06cc84bd450a7f08bbd3ff7d
AkashItlogiko <akashsahanwu089@gmail.com>
2025-06-01 15:36:32 +0600

    login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './components /Login';
 
 function App() {
   return (
-    <div className="text-white h-[100vh] flex justify-center bg-cover" style={{background:"url('../src/assets/react.jpg')"}}>
+    <div className="text-white h-[100vh] flex items-center justify-center bg-cover" style={{background:"url('../src/assets/react.jpg')"}}>
        <Login/>
      
     </div>
