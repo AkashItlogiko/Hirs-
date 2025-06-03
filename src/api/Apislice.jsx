23a72daa031ce9b37aeaa2fd0d66877f86544e0a
AkashItlogiko <akashsahanwu089@gmail.com>
2025-06-03 12:52:05 +0600

    Login success

--- src/api/Apislice.jsx
+++ src/api/Apislice.jsx
@@ -1,21 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const apiAuth=createApi({
-     reducerPath:'apiAuth',
-      baseQuery:fetchBaseQuery({
-            baseUrl:'http://127.0.0.1:8001',     
+export const apiAuth = createApi({
+  reducerPath: 'apiAuth',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8000/api/',
+  }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        method: 'POST',
+        body: data,
+        url: '/login',
       }),
-       endpoints:(builder)=>({
-              login: builder.mutation({
-                query: (data) => ({
-                 mathod: 'POST',
-                  body : data,
-                  url :'/login',
-                }),
-
-              }),    
-            }) 
-   
+    }),
+  }),
 });
 
- 
+export const { useLoginMutation } = apiAuth;
