c0dc3d9cfe4897f9c72f37aaf7e63ee915c91858
AkashItlogiko <akashsahanwu089@gmail.com>
2025-06-25 09:56:07 +0600

    Now comment

--- src/api/Apislice.jsx
+++ src/api/Apislice.jsx
@@ -22,59 +22,59 @@ export const apiAuth = createApi({
         },
       }),
     }),
-    getEmployees: builder.query({
-      query: () => '/employees',
-    }),
-    deleteEmployee: builder.mutation({
-      query: (id) => ({
-        method: 'DELETE',
-        url: `/employees/${id}`,
-      }),
-    }),
-    createEmployee: builder.mutation({
-      query: (employeeData) => ({
-        method: 'POST',
-        url: '/employees',
-        body: employeeData,
-      }),
-    }),
-    getAttendance: builder.query({
-      query: () => '/attendance',
-    }),
-    deleteAttendance: builder.mutation({
-      query: (id) => ({
-        method: 'DELETE',
-        url: `/attendance/${id}`,
-      }),
-    }),
-    getSalaries: builder.query({
-      query: () => '/salaries',
-    }),
-    deleteSalary: builder.mutation({
-      query: (id) => ({
-        method: 'DELETE',
-        url: `/salaries/${id}`,
-      }),
-    }),
-    createSalary: builder.mutation({  
-      query: (salaryData) => ({
-        method: 'POST',
-        url: '/salaries',
-        body: salaryData,
-      }),
-    }),
+    // getEmployees: builder.query({
+    //   query: () => '/employees',
+    // }),
+    // deleteEmployee: builder.mutation({
+    //   query: (id) => ({
+    //     method: 'DELETE',
+    //     url: `/employees/${id}`,
+    //   }),
+    // }),
+    // createEmployee: builder.mutation({
+    //   query: (employeeData) => ({
+    //     method: 'POST',
+    //     url: '/employees',
+    //     body: employeeData,
+    //   }),
+    // }),
+    // getAttendance: builder.query({
+    //   query: () => '/attendance',
+    // }),
+    // deleteAttendance: builder.mutation({
+    //   query: (id) => ({
+    //     method: 'DELETE',
+    //     url: `/attendance/${id}`,
+    //   }),
+    // }),
+    // getSalaries: builder.query({
+    //   query: () => '/salaries',
+    // }),
+    // deleteSalary: builder.mutation({
+    //   query: (id) => ({
+    //     method: 'DELETE',
+    //     url: `/salaries/${id}`,
+    //   }),
+    // }),
+    // createSalary: builder.mutation({  
+    //   query: (salaryData) => ({
+    //     method: 'POST',
+    //     url: '/salaries',
+    //     body: salaryData,
+    //   }),
+    // }),
   }),
 });
 
 export const {
   useLoginMutation,
   useLogOutMutation,
-  useGetEmployeesQuery,
-  useDeleteEmployeeMutation,
-  useCreateEmployeeMutation,
-  useGetAttendanceQuery,
-  useDeleteAttendanceMutation,
-  useGetSalariesQuery,
-  useDeleteSalaryMutation,
-  useCreateSalaryMutation,  
+  // useGetEmployeesQuery,
+  // useDeleteEmployeeMutation,
+  // useCreateEmployeeMutation,
+  // useGetAttendanceQuery,
+  // useDeleteAttendanceMutation,
+  // useGetSalariesQuery,
+  // useDeleteSalaryMutation,
+  // useCreateSalaryMutation,  
 } = apiAuth;
